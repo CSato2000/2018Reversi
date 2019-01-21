db0ac931d8b84f6e157016568110cab33213061f
Mellmag <yda.tm.2360623@gmail.com>
2019-01-21 13:56:20 +0900

    [update]キーボード操作処理の追加

--- GlobalNum.c
+++ GlobalNum.c
@@ -5,3 +5,5 @@ blackframe;  //黒のコマの数
 whiteframe;  //白のコマの数
 
 sMASS MassData[10][10]; //各マスのデータ配列
+
+POINT keyPos = { 0 };
--- KeyController.c
+++ KeyController.c
@@ -1,6 +1,23 @@
 #include<Windows.h>
 #include "KeyController.h"
 
-int KeyController(WPARAM pos) {
+int KeyController(WPARAM code) {
+
+	if (code == VK_LEFT)keyPos.x--;
+	if (code == VK_RIGHT)keyPos.x++;
+	if (code == VK_UP)keyPos.y--;
+	if (code == VK_DOWN)keyPos.y++;
+
+	if (keyPos.x < 0)keyPos.x = 0;
+	if (keyPos.x > 7)keyPos.x = 7;
+	if (keyPos.y < 0)keyPos.y = 0;
+	if (keyPos.y > 7)keyPos.y = 7;
+
+	if (code == VK_SPACE && MassData[keyPos.x][keyPos.y].putflag == 1) {
+		MassData[keyPos.x][keyPos.y].type = turn;
+		//Ç±Ç±Ç…ÉRÉ}ÇíuÇ≠ä÷êîÇåƒÇ‘
+		
+	}
+
 	return 0;
 }
