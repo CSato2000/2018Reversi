dd720447dd4064e920f25e9d318100f5d616cd65
Mellmag <yda.tm.2360623@gmail.com>
2019-01-23 13:57:45 +0900

    [update] おけるマスの表示

--- BoardDraw.c
+++ BoardDraw.c
@@ -5,7 +5,7 @@ void BoardDraw(HWND hWnd) {
 	//宣言
 	static HDC hdc;
 	static PAINTSTRUCT ps;
-	static HBRUSH hBrush[4];
+	static HBRUSH hBrush[5];
 	static initFlag;
 	
 	if (initFlag == 0) {
@@ -13,6 +13,7 @@ void BoardDraw(HWND hWnd) {
 		hBrush[1] = CreateSolidBrush(RGB(0xFF, 0xFF, 0xFF));    // WHITE
 		hBrush[2] = CreateSolidBrush(RGB(0, 0, 0));		        // BLACK
 		hBrush[3] = CreateSolidBrush(RGB(0xAA, 0xAA, 0xAA));
+		hBrush[4] = CreateSolidBrush(RGB(0, 0x88, 0));
 		initFlag = 1;
 	}
 
@@ -39,9 +40,20 @@ void BoardDraw(HWND hWnd) {
 		LineTo(hdc, 400, 50 * (i + 1));
 	}
 
+	//置けるマスを表示
+	SelectObject(hdc, hBrush[4]);
+	for (int i = 1; i <= 8; i++) {
+		for (int j = 1; j <= 8; j++) {
+			if (MassData[i][j].putflag == 1) {
+				Rectangle(hdc, (i - 1) * 50, (j - 1) * 50, i * 50 + 1, j * 50 + 1);
+			}
+		}
+	}
+
 	//キーの選択位置
 	SelectObject(hdc, hBrush[3]);
 	Rectangle(hdc, keyPos.x * 50, keyPos.y * 50, (keyPos.x + 1) * 50 + 1, (keyPos.y + 1) * 50 + 1);
+
 
 	//小さな黒丸の描画
 	SelectObject(hdc, hBrush[2]);
