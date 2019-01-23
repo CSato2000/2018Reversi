18318561b30efbee9c40a57d38ddd048b5305f30
CSato2000 <bambeaman334@gmail.com>
2019-01-23 13:24:02 +0900

    [update]SearchMouseの呼び出し
    呼び出しました

--- WinMain.c
+++ WinMain.c
@@ -50,7 +50,8 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wp, LPARAM lp) {
 		break;
 
 	case WM_LBUTTONDOWN:	//マウス左ボタンが押された
-
+		//SearchMouse(POINT pos);
+		InvalidateRect(hwnd, NULL, FALSE);
 		break;
 
 	case WM_KEYDOWN:	//キーが押された
