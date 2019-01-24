ffc356ac8a6cc9844d296be4d41c7fbda3c966ad
Mellmag <yda.tm.2360623@gmail.com>
2019-01-24 10:29:03 +0900

    [update] 多重インクルード防止文追加

--- BoardDraw.c
+++ BoardDraw.c
@@ -70,8 +70,8 @@ void BoardDraw(HWND hWnd) {
 	whiteframe = 0;
 
 	// ÉIÉZÉçî’ÇÃï\é¶
-	for (int i = 1; i <= 8; i++) {
-		for (int j = 1; j <= 8; j++) {
+	for (int i = 1; i < 9; i++) {
+		for (int j = 1; j < 9; j++) {
 			if (MassData[i][j].type != EMPTY) {
 				if (MassData[i][j].type == WHITE) {
 					SelectObject(hdc, hBrush[1]);  //îí
