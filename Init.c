a3aa667bf2f2fb8fa4bffb2a99e96d700bc2a918
Yukiya1027 <yukiya1027yda@gmail.com>
2019-01-28 13:54:18 +0900

    [update] 少し仕様変更

--- Init.c
+++ Init.c
@@ -7,10 +7,10 @@ void Init() {
     whiteframe = 2;
 
 	memset(MassData, 0x00, sizeof*(MassData));
-	MassData[4][4].type = BLACK;
-	MassData[5][5].type = BLACK;
-	MassData[4][5].type = WHITE;
-	MassData[5][4].type = WHITE;
+	MassData[4][4].type = WHITE;
+	MassData[5][5].type = WHITE;
+	MassData[4][5].type = BLACK;
+	MassData[5][4].type = BLACK;
 
 	AllCheckPut();
 
