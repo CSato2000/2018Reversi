38e15ef7c5ac2960860be410899bde139e30edbc
tajima <tajima.a.work@gmail.com>
2019-02-04 13:49:36 +0900

    [update]UI改良～～～
    ターンと勝敗の自己主張を激しくしました

--- UI.c
+++ UI.c
@@ -4,6 +4,7 @@ void UI(hWnd) {
 
 	//ハンドルの取得
 	HDC hdc;
+	HFONT hFont;
 	hdc = GetDC(hWnd);
 
 	//文字列
@@ -13,39 +14,62 @@ void UI(hWnd) {
 	LPTSTR lptStr4 = TEXT("白の勝ちです");
 	LPTSTR lptStr5 = TEXT("引き分けです");
 	LPTSTR lptStr6 = TEXT("　　　　　　");
-	LPTSTR lptStr7 = TEXT("再戦：SPACE");
+	LPTSTR lptStr7 = TEXT("再戦：ENTER");
 
+	//フォントを生成
+	hFont = CreateFont(
+		40, 0, 0, 0, FW_BOLD, TRUE, FALSE, FALSE,
+		SHIFTJIS_CHARSET, OUT_DEFAULT_PRECIS,
+		CLIP_DEFAULT_PRECIS, DEFAULT_QUALITY,
+		VARIABLE_PITCH | FF_ROMAN, NULL
+	);
 
+	//くそでかフォント
+	SelectObject(hdc, hFont);
 
 	//ターン表示
 	if (turn == BLACK)TextOut(hdc, 460, 10, lptStr1, lstrlen(lptStr1));
 	if (turn == WHITE)TextOut(hdc, 460, 10, lptStr2, lstrlen(lptStr2));
 
+	//普通のフォント
+	SelectObject(hdc, GetStockObject(SYSTEM_FONT));
+
 	if (sceneFlag == RESULT) {
+
+
 		//勝利者表示
-		if (GetWinner() == BLACK) { TextOut(hdc, 460, 110, lptStr3, lstrlen(lptStr3)); }
-		if (GetWinner() == WHITE) { TextOut(hdc, 460, 110, lptStr4, lstrlen(lptStr4)); }
-		if (GetWinner() == DRAW) { TextOut(hdc, 460, 110, lptStr5, lstrlen(lptStr5)); }
+		//くそでかフォント
+		SelectObject(hdc, hFont);
+
+		if (GetWinner() == BLACK) { TextOut(hdc, 460, 210, lptStr3, lstrlen(lptStr3)); }
+		if (GetWinner() == WHITE) { TextOut(hdc, 460, 210, lptStr4, lstrlen(lptStr4)); }
+		if (GetWinner() == DRAW) { TextOut(hdc, 460, 210, lptStr5, lstrlen(lptStr5)); }
+
+		SelectObject(hdc, GetStockObject(SYSTEM_FONT));
 
 		TextOut(hdc, 460, 310, lptStr7, lstrlen(lptStr7));
+
 	}
 	else {
-		TextOut(hdc, 460, 110, lptStr6, lstrlen(lptStr6));
+		TextOut(hdc, 460, 210, lptStr6, lstrlen(lptStr6));
 		TextOut(hdc, 460, 310, lptStr6, lstrlen(lptStr6));
 	}
+
+	//くそでかフォントを削除
+	DeleteObject(hFont);
 
 	
 
 	//駒数表示
 	TCHAR buf[40];
 	int length = wsprintf(buf, TEXT("黒の総数:%2d "), blackframe);
-	TextOut(hdc, 480, 30, buf, length);
+	TextOut(hdc, 480, 80, buf, length);
 
 	length = wsprintf(buf, TEXT("白の総数:%2d "), whiteframe);
-	TextOut(hdc, 480, 50, buf, length);
+	TextOut(hdc, 480, 100, buf, length);
 
 	length = wsprintf(buf, TEXT("駒の総数:%2d "), blackframe + whiteframe);
-	TextOut(hdc, 480, 70, buf, length);
+	TextOut(hdc, 480, 120, buf, length);
 
 	//ハンドルの解放
 	ReleaseDC(hWnd,hdc);
